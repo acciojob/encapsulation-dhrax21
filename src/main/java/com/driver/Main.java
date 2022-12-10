2a771805df412a5c9e4412ecf433720a3e27573f
Dheeraj Singh <dhrax21bc@gmail.com>
2022-12-10 09:43:29 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly rw=new RWOnly();
+
+//        rw.name="djs";      // 'name' has private access in 'com.driver.RWOnly'
+
+        rw.setName("HP");
+        System.out.println(rw.getName());
+    }
 }
